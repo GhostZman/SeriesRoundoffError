215467124448578c34fb8c52d32186b5a9b3247a
Zachary Ives <zives@hawk.iit.edu>
2024-02-09 14:17:49 -0600

    Initial Commit

---
+++ SeriesRoundoffError/SeriesRoundoffErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriesRoundoffErrorApp.swift
+//  SeriesRoundoffError
+//
+//  Created by Phys440Zachary on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SeriesRoundoffErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
